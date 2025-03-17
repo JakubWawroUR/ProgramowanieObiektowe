be24ae6adfd4735d78d15fc4e690a1f462e700af
JakubWawroUR <jw134984@stud.ur.edu.pl>
2025-03-17 11:49:23 +0100

    test

--- Lab6/src/Main.java
+++ Lab6/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Punkt punkt1 = new Punkt(2.5,4.3);
         punkt1.opis();
         punkt1.przesun(2,12);
-        punkt1.opis();
+        punkt1.opis();d
         Figura figura = new Figura();
         Prostokat prostokat = new Prostokat(10.0,5.5);
         Trojkat trojkat = new Trojkat(12.3,5);
